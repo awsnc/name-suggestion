cb658d6582e5c90591c427a6ac247f9de3a7029a
awsnc <48656135+awsnc@users.noreply.github.com>
2019-03-18 22:10:38 -0400

    Add files via upload

---
+++ name suggestion.py
@@ -0,0 +1,21 @@
+print ('~~ This project to suggest names for Aws ~~ ')
+print ('~~ Note: You need to suggest 5 names and computer will randamly pick name from the one you suggested ~~ ')
+first1=input (  "suggest First name #1: ")
+first2=input (  "suggest First name #2: ")
+first3=input (  "suggest First name #3: ")
+first4=input (  "suggest First name #4: ")
+first5=input (  "suggest First name #5: ")
+
+last1= input ('suggest Last name#1: ')
+last2= input ('suggest Last name#2: ')
+last3= input ('suggest Last name#3: ')
+last4= input ('suggest Last name#4: ')
+last5= input ('suggest Last name#5: ')
+
+import random
+
+
+first= random.choice(first1,first5)
+last=random.choice((last1,last5)
+
+
